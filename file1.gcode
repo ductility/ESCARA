48c5b7b2221a26f12f714475e745e6989c6bf95b
ductility <jys9704@gmail.com>
2019-11-10 11:11:10 +0900

    first commit

---
+++ file1.gcode
@@ -0,0 +1,16 @@
+(Scribbled version of C:\Users\정연성\AppData\Local\Temp\ink_ext_XXXXXX.svgSU6WA0 @ 2000.00)
+( unicorn.py --tab="homing" --pen-up-angle=50 --pen-down-angle=30 --start-delay=150 --stop-delay=150 --xy-feedrate=2000 --z-feedrate=150 --z-height=0 --finished-height=0 --register-pen=true --x-home=0 --y-home=0 --num-copies=1 --continuous=false --pause-on-layer-change=false C:\Users\정연성\AppData\Local\Temp\ink_ext_XXXXXX.svgSU6WA0 )
+G92 X0.00 Y0.00 Z0.00 (you are here)
+M3 (pen down)
+M5 (pen up)
+(Polyline consisting of 8 segments.)
+G1 X65.67 Y140.74 F2000.00
+M3 (pen down)
+G1 X64.37 Y140.67 F2000.00
+G1 X66.08 Y79.73 F2000.00
+M5 (pen up)
+(end of print job)
+M5 (pen up)
+G1 X0 Y0 F2000.00
+G1 Z0.00 F150.00 (go up to finished level)
+G1 X0.00 Y0.00 F2000.00 (go home)
---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var path = require('path');
+
+var server = http.createServer(app);
+
+var port = 3000;
+app.listen(port, function(){
+	console.log("Express server has started on port",port);
+});
+
+
+// 시리얼 포트 설정
+
+// COM6 : 아두이노가 연결된 포트
+
+var serialPort  = require('serialport');
+
+//시리얼포트 연결되어있는지 확인하고 자동으로 설정해주는 예제,,, 잘안되는듯
+// serialPort.list(function(err, ports) {
+// 	ports.forEach(function(port) {
+// 		console.log("comName : ", port.comName);
+//     });
+//     err.message("Port not found");
+// });
+
+var ArdoinoPort = new serialPort('COM3',{
+    baudRate : 115200,
+    // defaults for Arduino serial communication
+    dataBits : 8,
+    parity : 'none',
+    stopBits: 1,
+    flowControl: false,
+})
+
+ArdoinoPort.on('open', function () {
+    console.log('open serial communication');
+})
+
+
+
+// view디렉터리 설정, ejs엔진사용설정.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// public디렉터리 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//랜더링
+app.get('/',function(req,res) {
+    res.status(200).render('index.ejs');
+})
+app.get('/joystick',function(req,res) {
+    res.status(200).render('joystick.ejs');
+})
+app.get('/image',function(req,res) {
+    res.status(200).render('image.ejs');
+})
+
+
+//시리얼 송신
+app.get('/index/:id',function(req,res){
+    console.log(req.params.id);
+    ArdoinoPort.write(req.params.id+'\n') ;
+    res.status(200).send('Controlled, OK!!');
+})
+app.get('/joystick/:id',function(req,res){
+    console.log(req.params.id);
+    ArdoinoPort.write(req.params.id+'\n') ;
+    res.status(200).send('Controlled, OK!!');
+})
+
+
+//파일읽기
+var fs = require('fs');
+fs.readFile('file1.gcode', 'utf8', function(err, data){
+    app.get('/image/file1',function(req,res){
+        console.log(data);
+        ArdoinoPort.write(data) ;
+        res.status(200).send('Controlled, OK!!');
+    })
+});
+fs.readFile('file2.gcode', 'utf8', function(err, data){
+    app.get('/image/file2',function(req,res){
+        console.log(data);
+        ArdoinoPort.write(data) ;
+        res.status(200).send('Controlled, OK!!');
+    })
+});
+fs.readFile('file3.gcode', 'utf8', function(err, data){
+    app.get('/image/file3',function(req,res){
+        console.log(data);
+        ArdoinoPort.write(data) ;
+        res.status(200).send('Controlled, OK!!');
+    })
+});
+fs.readFile('file4.gcode', 'utf8', function(err, data){
+    app.get('/image/file4',function(req,res){
+        console.log(data);
+        ArdoinoPort.write(data) ;
+        res.status(200).send('Controlled, OK!!');
+    })
+});
